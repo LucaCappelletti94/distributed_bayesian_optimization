832d1e8b4f99754111e812bcdf5db1fa58db961c
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-22 19:04:16 +0200

    Updated setup.sh for travis

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Installing virtual env
+python -m pip install virtualenv
+# Setupping the virtualend
+python -m venv ray_venv
 # Cloning ray, getting my own version up until
 # they accept the latest pull request.
 git clone https://github.com/LucaCappelletti94/ray
@@ -8,10 +12,6 @@ cd ray
 git checkout random_search_bo
 # Making sure that this is the last version
 git pull
-# Installing virtual env
-python -m pip install virtualenv
-# Setupping the virtualend
-python -m venv ray_venv
 # Install Ray
 python python/ray/setup-dev.py --yes
 # Navigating back out
