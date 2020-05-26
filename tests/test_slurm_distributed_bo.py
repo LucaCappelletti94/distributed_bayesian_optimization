47cc21d088365d0ba5a33a3df3479a303cb4acd0
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-26 09:59:27 +0200

    Updated testing

--- tests/test_slurm_distributed_bo.py
+++ tests/test_slurm_distributed_bo.py
@@ -1,11 +1,13 @@
 import os
 from tqdm.auto import tqdm
 import subprocess
+import time
 
 
 def test_slurm_distributed_bo():
     subprocess.run(["sbatch", "bayesian_test.sh"])
     for _ in tqdm(range(120)):
+        time.sleep(1)
         if os.path.exists("BO.csv"):
             break
     assert os.path.exists("BO.csv")
