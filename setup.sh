b6f626356efc915b3a8507bf07901927a0fb9a76
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-05-26 13:07:46 +0200

    Patching errors

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python -m pip install click
-# Installing latest Ray from master
-python -m pip install -U https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-0.9.0.dev0-cp36-cp36m-manylinux1_x86_64.whl
 # Installing additional requirements
 python -m pip install -r requirements.txt
+# Installing latest Ray from master
+python -m pip install -U https://s3-us-west-2.amazonaws.com/ray-wheels/latest/ray-0.9.0.dev0-cp36-cp36m-manylinux1_x86_64.whl
